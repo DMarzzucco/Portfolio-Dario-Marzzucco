ce9d22c9cbb48f1e87ca9411236f7ce1f141954d
DMarzzucco <drrkermazyxokv@gmail.com>
2023-11-25 13:38:09 -0300

    update

---
+++ sor/script.js
@@ -0,0 +1,26 @@
+//nav  
+let menuVisible = false;
+function showAndHideM (){
+    if (menuVisible){
+        document.getElementById("nav").classList="";
+        menuVisible=false;
+    }else{
+        document.getElementById("nav").classList="responsive";
+        menuVisible=true;
+    }
+}
+function selecionar(){
+    document.getElementById("nav").classList ="";
+    menuVisible=false;
+}
+//tab about
+function openTab(tabName) {
+    // Ocultar todas las pestañas
+    var tabs = document.querySelectorAll('.tab');
+    tabs.forEach(function(tab) {
+        tab.classList.remove('active');
+    });
+
+    // Mostrar la pestaña seleccionada
+    document.getElementById(tabName).classList.add('active');
+}
