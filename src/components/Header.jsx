aab51132a1c3b2fd980aedba2a6355e88be9b6ad
DMarzzucco <drrkermazyxokv@gmail.com>
2024-02-13 16:17:30 -0300

    fresh

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,13 @@
 import react from "react";
 import { Link } from "react-router-dom";
 import "./Navbar.css";
+import { useState } from "react";
 
 function Header() {
+    const [menuOpen, setMenuOpen] = useState(false);
+    const toggleMenu = () => {
+        setMenuOpen(!menuOpen);
+    };
     return (
         <>
             <header>
@@ -14,6 +19,25 @@ function Header() {
                             marginLeft: '20px'
                         }} />
                     </Link>
+                    <div className="n-r">
+                        <div className="nav-respon" onClick={toggleMenu}>
+                            <i className="fa-solid fa-ellipsis-vertical" style={{
+                                color: '#fff',
+                            }}></i>
+                        </div>
+                        {menuOpen && (
+                            <div className="menu-respon">
+                                <ul className="nav-list">
+                                    <Link to="/About">
+                                        Acerca de mi
+                                    </Link>
+                                    <Link to="/Ubication">
+                                        Turno
+                                    </Link>
+                                </ul>
+                            </div>
+                        )}
+                    </div>
                 </div>
                 {/* nav */}
                 <nav>
@@ -27,11 +51,7 @@ function Header() {
                     </ul>
                 </nav>
                 {/* respon */}
-                <div className="nav-respon">
-                    <i className="fa-solid fa-ellipsis-vertical" style={{
-                        color: '#fff',
-                    }}></i>
-                </div>
+
             </header>
         </>
     )
