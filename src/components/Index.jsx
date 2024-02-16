c0d5c58330e326a6742189de6537cd3773df3bac
Dmarzzucco <drrkermazyxokv@gmail.com>
2024-02-16 18:32:45 -0300

    fresh

--- src/components/Index.jsx
+++ src/components/Index.jsx
@@ -8,46 +8,42 @@ function Index() {
             <header className='cabezal'>
                 <div className='logo'>
                     <a href=" "> <img src="/img/logo.png" alt="" style={{
-                        width: '328px',
+                        width: '305px',
                         height: '70px'
                     }}></img>
                     </a>
                 </div>
-                <section className='text-h'>
-                    <h2>Psicoterapia Individual</h2>
-                    <p> No hay salud, sin salud mental.</p>
-                </section>
-                {/*  */}
-                <section className="header-inicio">
-                    <div className="wave">
+                <section className="contenido">
+                    {/* nav */}
+                    <div className='text-h'>
+                        <h2>Psicoterapia Individual</h2>
+                        <p> No hay salud, sin salud mental.</p>
+                    </div>
+                    <div className='gr'>
+                        <div className="ph">
+                        </div>
+                        <div className="inf-gr">
+                            <div className="cn">
+                                <h1>Maricel Sovran</h1>
+                                <span>Licenciada en Piscología</span>
+                            </div>
+                            <div className="nav-i">
+                                <Link to='About' className="b-t">
+                                    <i className="fa-solid fa-user"></i>
+                                    Acerca de mí
+                                </Link>
+                                <Link to='Ubication' className="b-t">
+                                    <i className="fa-solid fa-phone"></i>
+                                    Turno
+                                </Link>
+                            </div>
+                        </div>
                     </div>
                 </section>
+
             </header>
             {/*  */}
-            <div className="contenido">
-                {/* nav */}
-                <div className='gr'>
-                    <div className="ph">
-                    </div>
-                    <div className="inf-gr">
-                        <div className="cn">
-                            <h1>Maricel Sovran</h1>
-                            <span>Licenciada en Piscología</span>
-                        </div>
-                        <div className="nav-i">
-                            <Link to='About' className="b-t">
-                                <i className="fa-solid fa-user"></i>
-                                Acerca de mí
-                            </Link>
-                            <a href="#">||</a>
-                            <Link to='Ubication' className="b-t">
-                                <i className="fa-solid fa-phone"></i>
-                                Turno
-                            </Link>
-                        </div>
-                    </div>
-                </div>
-            </div>
+
         </>
 
     )
